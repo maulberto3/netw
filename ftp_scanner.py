6f8d3f6e3f0e7201cdc8feb7271cf13155921a23
Mauricio <maulberto3@hotmail.com>
2021-08-22 00:03:41 -0600

    ftp_scanner

---
+++ ftp_scanner.py
@@ -0,0 +1,38 @@
+def main():
+
+    from ftplib import FTP
+    from pprint import pprint
+    from random import randint
+    from time import sleep
+    from datetime import datetime as dti
+
+    # SIMPLE FTP standard
+
+    import socket as s
+
+    def rand_adr():
+        # https://www.csestack.org/different-classes-of-ip-address/
+        return f'{randint(0,223)}.{randint(0,223)}.{randint(0,223)}.{randint(0,223)}'
+
+    while True:
+        # kill in unix with ps aux | grep <name of script> && kill <PID>
+        print()
+        adr = rand_adr()
+        print(f'Trying {adr}...')
+        try:
+            with FTP(
+                    adr, timeout=0.5
+            ) as ftp_conn:  # connect to host, default port  # ftp.us.debian.org
+                ftp_conn.set_debuglevel(2)
+                print(ftp_conn.getwelcome())
+                # ftp_conn.login()  # user anonymous, passwd anonymous@
+                # ftp_conn.dir()
+            with open('ftp_ok_anon_hosts.txt', 'a') as file:
+                file.write(adr + '\n' + ' ' +
+                           dti.now().strftime('%b-%d-%Y %T'))
+        except BaseException as e:
+            print(e)
+
+
+if __name__ == '__main__':
+    main()
--- ftp_standard.py
+++ ftp_standard.py
@@ -1,11 +1,37 @@
 from ftplib import FTP
+from pprint import pprint
+from random import randint
+from time import sleep
 
 # SIMPLE FTP standard
 
-ftp = FTP('ftp.us.debian.org')  # connect to host, default port
-ftp.login()  # user anonymous, passwd anonymous@
-ftp.cwd('debian')  # change into "debian" directory
-ftp.retrlines('LIST')  # list directory contents
-with open('README', 'wb') as fp:
-    ftp.retrbinary('RETR README', fp.write)
-ftp.quit()
+import socket as s
+
+def rand_adr():
+    return f'{randint(0,223)}.{randint(0,223)}.{randint(0,223)}.{randint(0,223)}'
+
+with open('ftp_ok_hosts.txt', 'w+') as file:
+    file.write(randint())
+
+with open('ftp_ok_hosts.txt', 'w+') as file:
+    file.write(randint())
+
+
+
+for i in range(1000):
+    adr = rand_adr()
+    print(f'Trying {adr}...')
+    try:
+        with FTP(adr, timeout=1) as ftp_conn: # connect to host, default port  # ftp.us.debian.org
+            ftp_conn.login()  # user anonymous, passwd anonymous@
+            ftp_conn.dir()
+            # with open('ftp_ok_hosts.txt', 'w+')
+            # ftp.retrlines('LIST')  # list directory contents
+            # ftp.cwd('debian')  # change into "debian" directory
+            # ftp.nlst()
+            # with open('README', 'wb') as fp:
+            #     ftp.retrbinary('RETR README', fp.write)
+            # ftp.quit()
+    except BaseException as e:
+        print()
+        print(e)
